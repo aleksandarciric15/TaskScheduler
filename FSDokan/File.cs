d3cbfdcb98fc76ab0c3a8089e830cb2d8d466c8e
SlovenskaAntiteza <151884698+SlovenskaAntiteza@users.noreply.github.com>
2023-12-07 20:43:16 +0100

    Add files via upload

---
+++ FSDokan/File.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSDokan
+{
+    public class File
+    {
+        private byte[] data;
+        private string name;
+        private DateTime created;
+
+        public File(string name, DateTime created)
+        {
+            data = Array.Empty<byte>();
+            this.name = name;
+            this.created = created;
+        }
+
+        public byte[] Data { get { return data; } set { data = value; } }
+
+        public string Name { get { return name; } set { name = value; } }
+
+        public DateTime Created { get { return created; } set { created = value; } }
+    }
+}
---
+++ FSDokan/Program.cs
@@ -0,0 +1,50 @@
+﻿using FSDokan;
+using DokanNet;
+using DokanNet.Logging;
+using Scheduler;
+
+char driveLetter = 'Y';
+
+Scheduler.TaskScheduler scheduler = new FifoScheduler(1);
+Dictionary<string, bool> processedFiles = new Dictionary<string, bool>();
+
+
+Thread observingInput = new Thread(() =>
+{
+    while (true)
+    {
+        Thread.Sleep(1000);
+        string folder = $"{driveLetter}:\\" + "input\\";
+        var files = Directory.GetFiles(folder);
+        foreach (var file in files)
+        {
+            if (!processedFiles.ContainsKey(file))
+            {
+                Thread.Sleep(100);
+                List<Resource> resources = new List<Resource>();
+                resources.Add(new FileResource(file));
+                scheduler.Schedule(new ImageSharpeningTask(resources, "Y:\\output\\", 2));
+                processedFiles.Add(file, true);
+            }
+        }
+    }
+});
+observingInput.IsBackground = true;
+observingInput.Start();
+
+using (ConsoleLogger consoleLogger = new("[Dokan]"))
+using (Dokan dokan = new(consoleLogger))
+{
+    string mountPoint = $"{driveLetter}:\\";
+    SimpleFS myFs = new();
+    DokanInstanceBuilder dokanInstanceBuilder = new DokanInstanceBuilder(dokan)
+        .ConfigureLogger(() => consoleLogger)
+        .ConfigureOptions(options =>
+        {
+            options.Options = DokanOptions.DebugMode;
+            options.MountPoint = mountPoint;
+        });
+    using DokanInstance dokanInstance = dokanInstanceBuilder.Build(myFs);
+    Console.ReadLine();
+}
+
